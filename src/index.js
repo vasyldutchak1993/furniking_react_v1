180baa0ecb910e00906de02c6fba591ac9e5a4cd
vasuldutschak <vasuldutschak@mail.ru>
2024-03-04 12:18:24 +0100

    create theme and connect po template

--- src/index.js
+++ src/index.js
@@ -2,13 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import {BrowserRouter} from "react-router-dom";
+import {ThemeProvider} from "styled-components";
+import {theme} from "./theme/theme";
 
-const BASE_NAME='furniking_react_v1'
+const BASE_NAME = 'furniking_react_v1'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
         <BrowserRouter basename={BASE_NAME}>
-            <App/>
+            <ThemeProvider theme={theme}>
+                <App/>
+            </ThemeProvider>
         </BrowserRouter>
     </React.StrictMode>
 );
---
+++ src/theme/theme.js
@@ -0,0 +1,25 @@
+export const theme = Object.freeze({
+    colors: {
+        menuBg: '#F7F8FA',
+        green: '#7AC751',
+        primaryText: '#555555',
+        secondaryText: '#C2C2C2',
+        white: "#ffffff",
+        black: "#010101",
+        red: '#f44336',
+        blue: '#2196f3',
+    },
+    iconSize: {
+        sm: 16,
+        md: 24,
+        lg: 32
+    },
+    fontWeight: {
+        normal: '300',
+        bold: '500',
+        bolder: '700',
+        lighter: '100',
+        initial: "initial",
+        inherit: "inherit"
+    }
+})

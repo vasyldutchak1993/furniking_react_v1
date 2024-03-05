7efc509f6a7de202ed366b53255e459527997686
vasuldutschak <vasuldutschak@mail.ru>
2024-03-05 16:07:42 +0100

    add theme provider and curency context

--- src/index.js
+++ src/index.js
@@ -4,15 +4,18 @@ import App from './App';
 import {BrowserRouter} from "react-router-dom";
 import {ThemeProvider} from "styled-components";
 import {theme} from "./theme/theme";
+import CurrencyContext from "./contexts/currencyContext";
 
 const BASE_NAME = 'furniking_react_v1'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
         <BrowserRouter basename={BASE_NAME}>
-            <ThemeProvider theme={theme}>
-                <App/>
-            </ThemeProvider>
+            <CurrencyContext>
+                <ThemeProvider theme={theme}>
+                    <App/>
+                </ThemeProvider>
+            </CurrencyContext>
         </BrowserRouter>
     </React.StrictMode>
 );

c2ee79d8f19509f19404962514084e1cdba61558
vasuldutschak <vasuldutschak@mail.ru>
2024-03-05 17:09:58 +0100

    add footer config

---
+++ src/components/footer/footerConfig.js
@@ -0,0 +1,27 @@
+export const footerConfig=[
+    {
+        menuTitle:"Help",
+        menuItems:[
+            {href:'/privacy_policy',text:'Privacy Policy'},
+            {href:'/shipping_&_delivery',text:'Shipping & Delivery'},
+            {href:'/refund_policy',text:'Refund Policy'},
+            {href:'/track_your',text:'Track Your  Order'},
+        ]
+    },{
+        menuTitle:"Store",
+        menuItems:[
+            {href:'/privacy_policy',text:'Furniture'},
+            {href:'/tabble',text:'Tabble'},
+            {href:'/soffa',text:'Soffa'},
+            {href:'/other',text:'Other'},
+        ]
+    },{
+        menuTitle:"Supports",
+        menuItems:[
+            {href:'/feedbcak',text:'Feedbcak'},
+            {href:'/contact_us',text:'Contact us'},
+            {href:'/download_app',text:'Download app'},
+            {href:'/terms',text:'Terms conditins'},
+        ]
+    },
+]

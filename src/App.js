595cd95485b09f5a17154ac9acaf3b638186aff1
vasuldutschak <vasuldutschak@mail.ru>
2024-03-07 16:30:05 +0100

    add lazy load for home page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import {Route, Routes} from "react-router-dom";
 import Layout from "./layout/Layout";
+import {lazy} from "react";
+const HomePage=lazy(()=>import('./pages/HomePage'))
+
 
 function App() {
     return (
         <Routes>
             <Route path='/' element={<Layout/>}>
-                <Route index element={<h1>HOME PAGE</h1>}/>
+                <Route index element={<HomePage/>}/>
                 <Route path='shop' element={<h1>SHOP</h1>}/>
                 <Route path='blog' element={<h1>BLOG</h1>}/>
                 <Route path='about' element={<h1>About</h1>}/>

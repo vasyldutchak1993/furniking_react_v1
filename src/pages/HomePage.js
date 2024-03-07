f228af2306df96cc19bda16c979f27f449ddf951
vasuldutschak <vasuldutschak@mail.ru>
2024-03-07 16:32:46 +0100

    add to main page furniture section

---
+++ src/pages/HomePage.js
@@ -0,0 +1,13 @@
+import FeatureSection from "../components/feature_section/FeactureSection";
+import FurnitureSection from "../components/furniture_section/FurnitureSection";
+
+function HomePage(props) {
+    return (
+        <>
+            <FeatureSection/>
+            <FurnitureSection/>
+        </>
+    );
+}
+
+export default HomePage;

fec0122fe703c81de66840eb2dca1570f3150fe1
vasuldutschak <vasuldutschak@mail.ru>
2024-03-10 12:19:10 +0100

    add sections to main page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,11 +1,16 @@
 import FeatureSection from "../components/feature_section/FeactureSection";
 import FurnitureSection from "../components/furniture_section/FurnitureSection";
+import SectionTitle from "../components/section_title/SectionTitle";
+import SpecialOfferSection from "../components/special_offer_section/SpecialOfferSection";
+import ArticlesSection from "../components/articles_section/ArticlesSection";
 
 function HomePage(props) {
     return (
         <>
             <FeatureSection/>
             <FurnitureSection/>
+            <SpecialOfferSection/>
+            <ArticlesSection/>
         </>
     );
 }

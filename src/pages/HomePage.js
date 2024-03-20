eb4fce092a76fa6c01060dfcacf8bb0c5022fb04
vasuldutschak <vasuldutschak@mail.ru>
2024-03-20 19:18:49 +0100

    add reviews section to main page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,6 +3,8 @@ import FurnitureSection from "../components/furniture_section/FurnitureSection";
 import SpecialOfferSection from "../components/special_offer_section/SpecialOfferSection";
 import ArticlesSection from "../components/articles_section/ArticlesSection";
 import ProductsSection from "../components/products_section/ProductsSection";
+import ReviewsSection from "../components/reviews_section/ReviewsSection";
+import React from "react";
 
 function HomePage(props) {
     return (
@@ -10,8 +12,9 @@ function HomePage(props) {
             <FeatureSection/>
             <FurnitureSection/>
             <SpecialOfferSection/>
-            <ArticlesSection/>
             <ProductsSection/>
+            <ReviewsSection/>
+            <ArticlesSection/>
         </>
     );
 }

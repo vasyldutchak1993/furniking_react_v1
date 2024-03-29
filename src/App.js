e5e0b2f31f16d845e7815d96ceca2045945da0f8
vasuldutschak <vasuldutschak@mail.ru>
2024-03-29 17:05:09 +0100

    add rout to shop page

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import Layout from "./layout/Layout";
 import {lazy} from "react";
 const HomePage=lazy(()=>import('./pages/HomePage'))
 const BasketPage=lazy(()=>import('./pages/BasketPage'))
+const ShopPage=lazy(()=>import('./pages/ShopPage'))
 
 function App() {
     return (
         <Routes>
             <Route path='/' element={<Layout/>}>
                 <Route index element={<HomePage/>}/>
-                <Route path='shop' element={<h1>SHOP</h1>}/>
+                <Route path='shop' element={<ShopPage/>}/>
                 <Route path='blog' element={<h1>BLOG</h1>}/>
                 <Route path='about' element={<h1>About</h1>}/>
                 <Route path='contact' element={<h1>CONTACT US</h1>}/>

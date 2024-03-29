3db9b845a5dc0daaa504c6e797478239ff6c52f2
vasuldutschak <vasuldutschak@mail.ru>
2024-03-29 17:01:04 +0100

    add context basket to all app

--- src/index.js
+++ src/index.js
@@ -5,17 +5,20 @@ import {BrowserRouter} from "react-router-dom";
 import {ThemeProvider} from "styled-components";
 import {theme} from "./theme/theme";
 import CurrencyContext from "./contexts/currencyContext";
+import BasketContext from "./contexts/basketContext";
 
 const BASE_NAME = 'furniking_react_v1'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
         <BrowserRouter basename={BASE_NAME}>
-            <CurrencyContext>
-                <ThemeProvider theme={theme}>
-                    <App/>
-                </ThemeProvider>
-            </CurrencyContext>
+           <BasketContext>
+               <CurrencyContext>
+                   <ThemeProvider theme={theme}>
+                       <App/>
+                   </ThemeProvider>
+               </CurrencyContext>
+           </BasketContext>
         </BrowserRouter>
     </React.StrictMode>
 );

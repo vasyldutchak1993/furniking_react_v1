39d2457b6a9f25c65fe032b85ccb304364dfda81
vasuldutschak <vasuldutschak@mail.ru>
2024-04-13 12:52:28 +0200

    remove unused import

--- src/components/contact_form/ContactForm.js
+++ src/components/contact_form/ContactForm.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React from 'react';
 import {ContactFormContainer} from "./contactForm.styled";
 import {IoSend} from "react-icons/io5";
 import * as Yup from 'yup';

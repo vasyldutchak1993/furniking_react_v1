88c30ac54b47e6ed216b85755a2a9a72b23b14f0
vasuldutschak <vasuldutschak@mail.ru>
2024-04-13 12:22:39 +0200

    create About Page

---
+++ src/pages/AboutPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import SectionTitle from "../components/section_title/SectionTitle";
+
+function AboutPage(props) {
+    return (
+        <>
+            <SectionTitle>About US</SectionTitle>
+        </>
+    );
+}
+
+export default AboutPage;

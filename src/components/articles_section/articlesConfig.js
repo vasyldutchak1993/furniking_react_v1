9b9279bc6705affca37003d64a22e99aa9885217
vasuldutschak <vasuldutschak@mail.ru>
2024-04-13 12:06:14 +0200

    change href according to link

--- src/components/articles_section/articlesConfig.js
+++ src/components/articles_section/articlesConfig.js
@@ -3,8 +3,8 @@ import img2 from './../../assets/img/post_2.png'
 import img3 from './../../assets/img/post_3.png'
 import img4 from './../../assets/img/post_4.png'
 export const items =[
-    {id:1,type:"Furniture",date:"23 September 2022",title:"Begineer guide buying minimal sofa",href:"articles",img:img1},
-    {id:2,type:"TABLE",date:"23 September 2022",title:"Buying best minimal computer table",href:"articles",img:img2},
-    {id:3,type:"Bench",date:"23 march 2022",title:"How to choose best modern bench",href:"articles",img:img3},
-    {id:4,type:"Chair",date:"23 march 2022",title:"Best Summer Outfit Style in this Country",href:"articles",img:img4},
+    {id:1,type:"Furniture",date:"23 September 2022",title:"Begineer guide buying minimal sofa",href:"/blog",img:img1},
+    {id:2,type:"TABLE",date:"23 September 2022",title:"Buying best minimal computer table",href:"/blog",img:img2},
+    {id:3,type:"Bench",date:"23 march 2022",title:"How to choose best modern bench",href:"/blog",img:img3},
+    {id:4,type:"Chair",date:"23 march 2022",title:"Best Summer Outfit Style in this Country",href:"/blog",img:img4},
 ]

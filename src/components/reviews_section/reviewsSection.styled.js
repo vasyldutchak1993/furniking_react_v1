892f5c7c0910ee56fbbb95a9a9048c58e54596a3
vasuldutschak <vasuldutschak@mail.ru>
2024-04-13 12:45:04 +0200

    make it responsive and chahge style

--- src/components/reviews_section/reviewsSection.styled.js
+++ src/components/reviews_section/reviewsSection.styled.js
@@ -1,7 +1,12 @@
 import styled from "styled-components";
+import media from "styled-media-query";
+import {BREAK_POINTS} from "../../utils/breakPoints";
 
 export const SliderWrapper=styled.div`
     max-width: 900px;
     width: 100%;
     margin: 0 auto;
+    ${media.lessThan(BREAK_POINTS.MEDIUM)`
+        display:none;
+    `}
 `

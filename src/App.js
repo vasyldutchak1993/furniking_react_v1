af17318b3215e65191c612a9b2e69cb57f8c39a3
vasuldutschak <vasuldutschak@mail.ru>
2024-04-13 12:49:32 +0200

    add pages to main template

--- src/App.js
+++ src/App.js
@@ -2,20 +2,29 @@ import './App.css';
 import {Route, Routes} from "react-router-dom";
 import Layout from "./layout/Layout";
 import {lazy} from "react";
-const HomePage=lazy(()=>import('./pages/HomePage'))
-const BasketPage=lazy(()=>import('./pages/BasketPage'))
-const ShopPage=lazy(()=>import('./pages/ShopPage'))
 
+const HomePage = lazy(() => import('./pages/HomePage'))
+const BasketPage = lazy(() => import('./pages/BasketPage'))
+const ShopPage = lazy(() => import('./pages/ShopPage'))
+const FavoritePage = lazy(() => import('./pages/FavoritePage'))
+const ContactPage = lazy(() => import('./pages/ContactPage'))
+const BlogPage = lazy(() => import('./pages/BlogPage'))
+const AboutPage = lazy(() => import('./pages/AboutPage'))
+const BlogItemPage = lazy(() => import('./pages/BlogItemPage'))
+const ProductDetailPage=lazy(()=>import("./pages/ProductDetailPage"))
 function App() {
     return (
         <Routes>
             <Route path='/' element={<Layout/>}>
                 <Route index element={<HomePage/>}/>
                 <Route path='shop' element={<ShopPage/>}/>
-                <Route path='blog' element={<h1>BLOG</h1>}/>
-                <Route path='about' element={<h1>About</h1>}/>
-                <Route path='contact' element={<h1>CONTACT US</h1>}/>
+                <Route path='product/:id' element={<ProductDetailPage/>}/>
+                <Route path='blog' element={<BlogPage/>}/>
+                <Route path='blog/:id' element={<BlogItemPage/>}/>
+                <Route path='about' element={<AboutPage/>}/>
+                <Route path='contact' element={<ContactPage/>}/>
                 <Route path='basket' element={<BasketPage/>}/>
+                <Route path='favorite' element={<FavoritePage/>}/>
                 <Route path='*' element={<h1>404 NOT FOUND</h1>}/>
             </Route>
         </Routes>

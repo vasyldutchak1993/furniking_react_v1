a4523b809a0c53d00ee9c3d1574b79eeb8b6396b
vasuldutschak <vasuldutschak@mail.ru>
2024-04-13 16:36:42 +0200

    fix bug

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import LoginForm from "../components/login_form/LoginForm";
-import L from "../components/login_form/l";
 import SectionTitle from "../components/section_title/SectionTitle";
 
 function LoginPage(props) {

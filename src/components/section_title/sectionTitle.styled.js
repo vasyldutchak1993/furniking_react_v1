82cd262a974af1e3970356975b329d6ac65eebb8
vasuldutschak <vasuldutschak@mail.ru>
2024-04-13 12:38:06 +0200

    refactoring section title

--- src/components/section_title/sectionTitle.styled.js
+++ src/components/section_title/sectionTitle.styled.js
@@ -1,10 +1,16 @@
 import styled from "styled-components"
+import media from "styled-media-query";
+import {BREAK_POINTS} from "../../utils/breakPoints";
 
 export const SectionTitleStyled=styled.div`
-  font-family: "Mulish", sans-serif;
-  color: ${props => props.theme.colors.primaryText};
-  font-size: 36px;
-  font-weight:  ${props => props.theme.fontWeight.bolder};
-  text-align: center;
-  text-transform: uppercase;
+    font-family: "Mulish", sans-serif;
+    color: ${props => props.theme.colors.primaryText};
+    font-size: 36px;
+    font-weight: ${props => props.theme.fontWeight.bolder};
+    text-align: center;
+    text-transform: uppercase;
+    margin: 25px;
+    ${media.lessThan(BREAK_POINTS.MEDIUM)`
+        display:none;
+    `}
 `

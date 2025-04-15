645a38e9887927b4f97811d5ecc21683d6895a58
Vasyl Dutchak <code.genius.web.dev@gmail.com>
2025-04-15 10:03:35 +0200

    reformat code

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import BasketContext from "./contexts/basketContext";
 
 const BASE_NAME = 'furniking_react_v1'
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
     <React.StrictMode>
         <BrowserRouter basename={BASE_NAME}>
